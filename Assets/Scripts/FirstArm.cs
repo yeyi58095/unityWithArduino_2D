6bd9359d19df1cc792462c6042bb170b82d62052
yeyi58095 <141631770+yeyi58095@users.noreply.github.com>
2026-04-27 20:17:21 +0800

    update the parameters of the first arm (y-axis, up-side-down)

--- Assets/Scripts/FirstArm.cs
+++ Assets/Scripts/FirstArm.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class FirstArm : MonoBehaviour {
     public char controlAxis = 'Y';
+
+    [Header("Encoder Settings")]
+    public float impulsesPerRound = 2000f;
+
+    [Header("Unity Angle Calibration")]
     public float angleOffset = -180f;
+    public bool invertDirection = false;
 
     private float currentAngle = -180f;
 
@@ -16,14 +22,21 @@ public class FirstArm : MonoBehaviour {
             SerialManager.Instance.OnMotorDataReceived -= HandleMotorData;
     }
 
-    void HandleMotorData(char axis, float motorAngle) {
+    void HandleMotorData(char axis, float encoderImpulse) {
         if (axis != controlAxis) return;
+
+        float motorAngle = encoderImpulse / impulsesPerRound * 360f;
+
+        if (invertDirection) {
+            motorAngle = -motorAngle;
+        }
 
         currentAngle = motorAngle + angleOffset;
 
         float radius = (GetComponent<CapsuleCollider>().height * 0.5f) * transform.localScale.y;
 
         float rad = currentAngle * Mathf.Deg2Rad;
+
         transform.position = new Vector3(
             radius * Mathf.Cos(rad),
             radius * Mathf.Sin(rad),
